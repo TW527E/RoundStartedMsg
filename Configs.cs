11590ec14f0cf8830adaaf97da8bb81d18abd4bf
TW527E <37478187+TW527E@users.noreply.github.com>
2021-08-12 14:30:38 +0800

    Initial commit

---
+++ Configs.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+
+namespace RoundStartedMsg
+{
+    public sealed class Configs : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [System.ComponentModel.Description("How long does it last")]
+        public ushort HowLong { get; set; } = 5;
+        [System.ComponentModel.Description("When Round Started Message")]
+        public string OnRoundStarted { get; set; } = "<color=red>Go! Don't AFK!</color>";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using UnityEngine;
+using Server = Exiled.Events.Handlers.Server;
+
+namespace RoundStartedMsg
+{
+	public class RoundStartedMsg : Plugin<Configs>
+	{
+		public static RoundStartedMsg RoundStartedMsgRef { get; set; }
+
+		public EventHandler eventHandlers;
+
+		public RoundStartedMsg()
+		{
+			RoundStartedMsgRef = this;
+		}
+
+		public override void OnEnabled()
+		{
+			eventHandlers = new EventHandler();
+			Server.RoundStarted += eventHandlers.OnRoundStarted;
+		}
+
+		public override void OnDisabled()
+		{
+			Server.RoundStarted += eventHandlers.OnRoundStarted;
+			eventHandlers = null;
+		}
+	}
+}
