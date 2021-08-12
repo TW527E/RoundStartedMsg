835a7afa059cf3150560fbbcc1fbed5fc83a8ee6
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-08-12 21:34:06 +0200

    Update Configs.cs

--- Configs.cs
+++ Configs.cs
@@ -1,13 +1,14 @@
 ﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
 
 namespace RoundStartedMsg
 {
-    public sealed class Configs : IConfig
+    public class Configs : IConfig
     {
         public bool IsEnabled { get; set; } = true;
-        [System.ComponentModel.Description("How long does it last")]
+        [Description("How long does it last")]
         public ushort HowLong { get; set; } = 5;
-        [System.ComponentModel.Description("When Round Started Message")]
+        [Description("When Round Started Message")]
         public string OnRoundStarted { get; set; } = "<color=red>Go! Don't AFK!</color>";
     }
 }
