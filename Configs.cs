a97c2ce8245a41d90666b290ef08f7dc998fcdc2
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-08-12 21:47:06 +0200

    Noice

--- Configs.cs
+++ Configs.cs
@@ -1,14 +1,18 @@
-﻿using Exiled.API.Interfaces;
+using Exiled.API.Interfaces;
 using System.ComponentModel;
 
 namespace RoundStartedMsg
 {
-    public class Configs : IConfig
+    public class Config : IConfig
     {
         public bool IsEnabled { get; set; } = true;
+
         [Description("How long does it last")]
         public ushort HowLong { get; set; } = 5;
+
         [Description("When Round Started Message")]
         public string OnRoundStarted { get; set; } = "<color=red>Go! Don't AFK!</color>";
     }
+}
+
 }
